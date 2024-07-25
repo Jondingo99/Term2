aecb01aca662327208c189b17bc4fab437194092
Jondingo99 <jonathanpirone@hotmail.com>
2024-07-25 16:42:50 -0400

    Initial commit

---
+++ Source/Term2/Term2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Term2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Term2, "Term2" );
