b76ecdcca32e605e394673901561199060307b60
Jondingo99 <jonathanpirone@hotmail.com>
2024-07-25 20:54:03 -0400

    Added Controls
    This didn't take as long

---
+++ Source/Term2/Public/Term2PlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Term2PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERM2_API ATerm2PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
