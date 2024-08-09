1a723f6b0f3c76d28f0c5bfbd4b6901be9bc5495
Jondingo99 <jonathanpirone@hotmail.com>
2024-08-09 19:55:44 -0400

    Added gamestates
    Didn't add the BP ones yet

--- Source/Term2/Private/Term2GameModeBase.cpp
+++ Source/Term2/Private/Term2GameModeBase.cpp
@@ -3,6 +3,43 @@
 
 #include "Term2GameModeBase.h"
 
+ATerm2GameModeBase::ATerm2GameModeBase()
+{
+
+}
+
+void ATerm2GameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CurrentGameState = EGameState::Waiting;
+	DisplayCountdown();
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ATerm2GameModeBase::StartGame, GameCountdownDuration, false);
+}
+
+EGameState ATerm2GameModeBase::GetCurrentGameState()
+{
+	return CurrentGameState;
+}
+
+void ATerm2GameModeBase::PlayerReachedEnd()
+{
+	CurrentGameState = EGameState::GameOver;
+	//To do - Return to update widget
+
+	//To do - Return to control PlayerController Input State
+
+}
+
+void ATerm2GameModeBase::DisplayCountdown()
+{
+	//To do - Return and create, store, display widget
+}
+
+void ATerm2GameModeBase::StartGame()
+{
+	CurrentGameState = EGameState::Playing;
+}
 
 
 
--- Source/Term2/Public/Term2GameModeBase.h
+++ Source/Term2/Public/Term2GameModeBase.h
@@ -6,6 +6,17 @@
 #include "GameFramework/GameModeBase.h"
 #include "Term2GameModeBase.generated.h"
 
+//Enum to track the state of the game
+UENUM(BlueprintType)
+enum class EGameState : uint8
+{
+	NONE		UMETA(DisplayName = "NONE"),
+	Waiting		UMETA(DisplayName = "Waiting"),
+	Playing		UMETA(DisplayName = "Playing"),
+	Paused		UMETA(DisplayName = "Paused"),
+	GameOver	UMETA(DisplayName = "GameOver"),
+};
+
 /**
  * 
  */
@@ -13,8 +24,34 @@ UCLASS()
 class TERM2_API ATerm2GameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+
+	//Functions
+	ATerm2GameModeBase();
+
+	virtual void BeginPlay() override;
+
+	EGameState GetCurrentGameState();
+	void PlayerReachedEnd();
+
+private:
+
+	// VARS
+
+	// Create and set CurrentGameState to NONE. This will be tracked in the code file.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="States", meta = (AllowPrivateAccess = "true"))
+	EGameState CurrentGameState = EGameState::NONE;
+	//Countdown before gameplay
+	UPROPERTY(EditAnywhere, Category="Game Details")
+	float GameCountdownDuration = 4.0f;
+
+	FTimerHandle TimerHandle;
 	
+	//Return to add widget details
+
+	//FUNCTIONS
 	
-	
-	
+	void DisplayCountdown();
+	void StartGame();
 };
